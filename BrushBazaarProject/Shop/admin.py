1ea5021cf1441c9ea1bda626aede94d2c5a0151d
sreejith-0087 <sreejith.s.8700@gmail.com>
2025-01-10 11:28:02 +0530

    Setting Shop Page

--- BrushBazaarProject/Shop/admin.py
+++ BrushBazaarProject/Shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from . models import BrushBazaarProducts, Categories
 # Register your models here.
+
+
+class ItemAdmin(admin.ModelAdmin):
+    search_fields = ['product', 'artist', 'description', 'category__category']
+    list_display = ['product', 'artist', 'category', 'stock', 'price']
+    list_editable = ['stock', 'price']
+admin.site.register(BrushBazaarProducts, ItemAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"link":['category',]}
+admin.site.register(Categories, CategoryAdmin)
+
--- BrushBazaarProject/Shop/views.py
+++ BrushBazaarProject/Shop/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from itertools import product
 
+from django.shortcuts import render, redirect
+from unicodedata import category
+
+from . models import Categories, BrushBazaarProducts
+from django.core.paginator import Paginator
 # Create your views here.
+
 
 def Home(request):
     return render(request, 'Home/Index.html')
@@ -10,3 +16,16 @@ def About(request):
 
 def Contact(request):
     return render(request, 'Home/Contact.html')
+
+def Shop(request, link=None):
+    if link is not None:
+        cat = Categories.objects.get(link=link)
+        products = BrushBazaarProducts.objects.filter(category=cat.id)
+    else:
+        all_items = BrushBazaarProducts.objects.all()
+        paginator = Paginator(all_items, 9)
+        page_number = request.GET.get('page')
+        products = paginator.get_page(page_number)
+    return render(request, 'Shop/Shop.html', {'products': products})
+
+
